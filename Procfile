25f471924cbc47bc7f0ecf37cbfaaffea1836972
Renato Fabbri <renato.fabbri@gmail.com>
2014-06-24 19:55:38 -0300

    feito esqueleto do streamer p heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python herokutest.py 
---
+++ maccess.py
@@ -0,0 +1,42 @@
+class mdc:
+    u1="mongodb://foo:bar@ds061188.mongolab.com:61188/tw1"
+    u2="mongodb://foo:bar@ds037467.mongolab.com:37467/tw2"
+    u3="mongodb://foo:bar@ds037467.mongolab.com:37467/tw3"
+    u4="mongodb://foo:bar@ds035428.mongolab.com:35428/tw4"
+    u5="mongodb://foo:bar@ds037607.mongolab.com:37607/tw5"
+class dbc:
+    h="mysql15.teia.org.br"# your host, usually localhost
+    u="teia16"             # your username
+    #p="pAAinelAAweb"         # your password
+    p="wuuPh67ee5ru"         # your password
+    d="teia16"               # name of the data base
+#TWITTER_API_KEY             
+#TWITTER_API_KEY_SECRET      
+#TWITTER_ACCESS_TOKEN        
+#TWITTER_ACCESS_TOKEN_SECRET 
+# todos do greenkobold
+# TWSS1
+# Twitter Search and Streamming - 1
+# aars.meteor.com
+class twss1:
+    tak='uI8OdZKgtJ46fOsrZS17WWtHU'
+    taks='5jYehAnT71Gg8GHBfRwAyzICGDs2c4Sr2TtITQ6ruyOPc3mQjx'
+    tat='2430470406-CU8OwLfbIaRCxBtpeR6KMMJRv51D2foYzSpGqYV'
+    tats='LCyO9ik57ihGu6IHyE4gTXNWmclVhL9PXedHFANbnKTE8'
+# TWSS2
+# Twitter Search and Streamming - 2
+# aars.meteor.com
+class twss2: # puxa AA
+    tak= "mmsnjhnmeqaFOUkUTC46ONDBZ"
+    taks="A0FS8xGk5GIHoaI8hzw5MJOyhgiD0B3vd6MRdiagDudZjbV0qD"
+    tat= "2430470406-M0spmbsyfz3G1qY7YCZYR0Ye38CXeHQxNfLyqQo"
+    tats="O5B9rbgbAB9Gghbsp3y9HB95DAfiNs7YaR3Cj7IsTG5cm"
+# TWSS3
+# Twitter Search and Streamming - s3
+# aars.meteor.com
+class twss3: # puxa arenaNETmundial
+    tak= "U3gkdcw144pb3H315Vsmphne5"
+    taks="jbuLKuamEaiNPXJfhfC9kaXYcoSSfRIgTldwuQYCcUJzEGNukU"
+    tat= "2430470406-45gX6ihMxnKQQmjX2yR1VoaTQIddgY5bT7OSOzT"
+    tats="bHS4NkMwBFaysdVqnsT25xhNzZwEbM64KPdpRDB6RqZ2Z"
+TW=[twss1,twss2,twss3]
