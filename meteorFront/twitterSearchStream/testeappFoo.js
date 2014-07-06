4be7575dbc12aa01d10565de5d04bdb0eab38ce6
Renato Fabbri <renato.fabbri@gmail.com>
2014-07-06 18:22:57 -0300

    fumegante e jah agilizando as queries corretas

--- meteorFront/twitterSearchStream/testeappFoo.js
+++ meteorFront/twitterSearchStream/testeappFoo.js
@@ -25,33 +25,58 @@ if (Meteor.isServer) {
 //    }
 //});
 var T = new TwitMaker({
-//    consumer_key:         'iub8wm03f4sI4EMvxgdoicwIe',
-//    consumer_secret:      'wlMxqHGSKjrKjhTPhQtkdR9bUnK7sZyjgsUEEcxDlHqoJCHO4D', 
-//    access_token:         '18882547-183eFHigoFCYlZDh4IUSkVtsF6WM0CbX6yvu0Ah3a',
-//    access_token_secret:  'YrWWp3GNudB7rXgHaj6cokJyXM6SW3GUTmO1tqoIYv6Y9'
-    consumer_key:         'kj0HQGII7R1tfsAXhUcq6g',
-    consumer_secret:      'LApqUqsuZmndSmOOt6erb6t72fCEe9B4BzSKCVIg', 
-    access_token:         '18882547-ZsbjCGH2XeCyAzxeCtVHAh1esPvKIiMWyarUyy1JX',
-    access_token_secret:  'BkW7shEXmTm7i7R9HbnP3ljGRfadzA1TBZNhHnDbmBGol'
+    consumer_key:         'iub8wm03f4sI4EMvxgdoicwIe',
+    consumer_secret:      'wlMxqHGSKjrKjhTPhQtkdR9bUnK7sZyjgsUEEcxDlHqoJCHO4D', 
+    access_token:         '18882547-183eFHigoFCYlZDh4IUSkVtsF6WM0CbX6yvu0Ah3a',
+    access_token_secret:  'YrWWp3GNudB7rXgHaj6cokJyXM6SW3GUTmO1tqoIYv6Y9'
+//    consumer_key:         'kj0HQGII7R1tfsAXhUcq6g',
+//    consumer_secret:      'LApqUqsuZmndSmOOt6erb6t72fCEe9B4BzSKCVIg', 
+//    access_token:         '18882547-ZsbjCGH2XeCyAzxeCtVHAh1esPvKIiMWyarUyy1JX',
+//    access_token_secret:  'BkW7shEXmTm7i7R9HbnP3ljGRfadzA1TBZNhHnDbmBGol'
 });
 
-termos=Configs.find({campo:"termos"}).fetch()[0].termos
-streama="";termos.forEach(function(i){if(i===termos[termos.length-1]){bar="";}else{bar=",";}streama+=i+bar;});
-var stream = T.stream('statuses/filter', { track: streama });
+termos=Configs.find({campo:"termos"}).fetch()[0].termos;
+streama="";
+termos.forEach(function(i){
+    bar=((i===termos[termos.length-1]) ? "" : "," );
+    streama+=i+bar;
+});
+stream = T.stream('statuses/filter', { track: streama });
 
-stream.on('tweet', function (tweet) {
+stream.on('tweet',               Meteor.bindEnvironment(
+function (tweet) {
   console.log(tweet);
-});
 
-
-
+                      Tweets.insert({tweet:tweet, termos:termos})
+},
+               function( error) {console.log( error);})
+                );
+//aa=Tweets.find({"tweet.user.screen_name":"BorogotoSfreba"}).fetch()
 Meteor.methods({
+    updateStream: function() {
+        termos=Configs.find({campo:"termos"}).fetch()[0].termos;
+        streama="";
+        termos.forEach(function(i){
+            bar=((i===termos[termos.length-1]) ? "" : "," );
+            streama+=i+bar;
+        });
+        delete stream;
+        stream = T.stream('statuses/filter', { track: streama });
+
+        stream.on('tweet',  Meteor.bindEnvironment(
+                    function (tweet) {
+                  console.log(tweet);
+                  Tweets.insert({tweet:tweet, termos:termos})
+              })
+        );
+    },
     searchTwitter: function(term) {
-        T.get('search/tweets', { q: term, count: 100 },
+//Meteor.call('searchTwitter', "#testeteste");
+        T.get('search/tweets', { q: term, count: 200 },
               Meteor.bindEnvironment(
                function(err, data, response) {
                     console.log(data);
-                    Tweets.insert(data.statuses);
+                    data.statuses.forEach(function(i){Tweets.insert(i);});
                   },
                function( error) {console.log( error);})
               );
@@ -59,15 +84,5 @@ Meteor.methods({
 });
 
   Meteor.startup(function () {
-    // code to run on server at startup
-//var T = new TwitMaker({
-//    consumer_key:         'iub8wm03f4sI4EMvxgdoicwIe',
-//    consumer_secret:      'wlMxqHGSKjrKjhTPhQtkdR9bUnK7sZyjgsUEEcxDlHqoJCHO4D', 
-//    access_token:         '18882547-183eFHigoFCYlZDh4IUSkVtsF6WM0CbX6yvu0Ah3a',
-//    access_token_secret:  'YrWWp3GNudB7rXgHaj6cokJyXM6SW3GUTmO1tqoIYv6Y9'
-//});
-
-
-
   });
 }
