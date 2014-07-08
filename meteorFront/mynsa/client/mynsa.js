1913c4b1f4d8023a25214711881bdf72db5ef7bc
Renato Fabbri <renato.fabbri@gmail.com>
2014-07-08 02:03:33 -0300

    feito caminho ateh o termo de interesse

--- meteorFront/mynsa/client/mynsa.js
+++ meteorFront/mynsa/client/mynsa.js
@@ -1,14 +1,25 @@
 Geral= new Meteor.Collection("geral");
 Session.set("screen","initialScreen"); // initial, dashboard, term
+  Template.termoSpecs.termo=function(){
+    return Session.get("termo");
+};
   Template.initialDashboard.observedTerms=function(){
     return Geral.findOne().termos_observados;
-}
+};
+  Template.main.termo= function () {
+    return (Session.get("screen")==="termoScreen");
+};
   Template.main.initial= function () {
     return (Session.get("screen")==="initialScreen");
 };
   Template.main.dashboard= function () {
     return (Session.get("screen")==="dashboardScreen");
 };
+  Template.termoSpecs.events({
+    'click #voltarDashboard': function(){
+        Session.set("screen","dashboardScreen");
+    },
+});
 
   Template.main.events({
     'click input': function () {
@@ -25,6 +36,16 @@ Session.set("screen","initialScreen"); // initial, dashboard, term
 
   Template.initialDashboard.events({
     'click .btn-default': function () {
-        console.log("adiciona");
+        novo_termo=$("#formGroupInputLarge").val();
+
+        itemDB={termo:novo_termo,stats:{n_msgs:0},adicionado_em:new Date(),primeira_msg_de:new Date()};
+        
+        Geral.update({_id:Geral.findOne()._id},{"$push":{"termos_observados":itemDB}});
+        $("#formGroupInputLarge").val("");
     },
+    'click .btn-success':function(){
+        tthis=this;
+        Session.set("termo",this.termo);
+        Session.set("screen","termoScreen");
+    }
   });
