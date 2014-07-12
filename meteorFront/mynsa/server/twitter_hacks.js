cb80fa37b79b64d0e7fcccc8522d08a997312278
Renato Fabbri <renato.fabbri@gmail.com>
2014-07-11 23:46:22 -0300

    modificada chave d twitter p deploy

--- meteorFront/mynsa/server/twitter_hacks.js
+++ meteorFront/mynsa/server/twitter_hacks.js
@@ -7,20 +7,20 @@ T = new TwitMaker({
 //    consumer_secret:      'LApqUqsuZmndSmOOt6erb6t72fCEe9B4BzSKCVIg', 
 //    access_token:         '18882547-ZsbjCGH2XeCyAzxeCtVHAh1esPvKIiMWyarUyy1JX',
 //    access_token_secret:  'BkW7shEXmTm7i7R9HbnP3ljGRfadzA1TBZNhHnDbmBGol'
-    consumer_key:         'uI8OdZKgtJ46fOsrZS17WWtHU',
-    consumer_secret:      '5jYehAnT71Gg8GHBfRwAyzICGDs2c4Sr2TtITQ6ruyOPc3mQjx',
-    access_token:         '2430470406-CU8OwLfbIaRCxBtpeR6KMMJRv51D2foYzSpGqYV',
-    access_token_secret:  'LCyO9ik57ihGu6IHyE4gTXNWmclVhL9PXedHFANbnKTE8'
+//    consumer_key:         'uI8OdZKgtJ46fOsrZS17WWtHU',
+//    consumer_secret:      '5jYehAnT71Gg8GHBfRwAyzICGDs2c4Sr2TtITQ6ruyOPc3mQjx',
+//    access_token:         '2430470406-CU8OwLfbIaRCxBtpeR6KMMJRv51D2foYzSpGqYV',
+//    access_token_secret:  'LCyO9ik57ihGu6IHyE4gTXNWmclVhL9PXedHFANbnKTE8'
     // deploy
-//    consumer_key:         'U3gkdcw144pb3H315Vsmphne5',
-//    consumer_secret:      'jbuLKuamEaiNPXJfhfC9kaXYcoSSfRIgTldwuQYCcUJzEGNukU', 
-//    access_token:         '2430470406-45gX6ihMxnKQQmjX2yR1VoaTQIddgY5bT7OSOzT',
-//    access_token_secret:  'bHS4NkMwBFaysdVqnsT25xhNzZwEbM64KPdpRDB6RqZ2Z'
+    consumer_key:         'U3gkdcw144pb3H315Vsmphne5',
+    consumer_secret:      'jbuLKuamEaiNPXJfhfC9kaXYcoSSfRIgTldwuQYCcUJzEGNukU', 
+    access_token:         '2430470406-45gX6ihMxnKQQmjX2yR1VoaTQIddgY5bT7OSOzT',
+    access_token_secret:  'bHS4NkMwBFaysdVqnsT25xhNzZwEbM64KPdpRDB6RqZ2Z'
     // deploy2
-    consumer_key:       "mmsnjhnmeqaFOUkUTC46ONDBZ",
-    consumer_secret:    "A0FS8xGk5GIHoaI8hzw5MJOyhgiD0B3vd6MRdiagDudZjbV0qD",
-    access_token:       "2430470406-M0spmbsyfz3G1qY7YCZYR0Ye38CXeHQxNfLyqQo",
-    access_token_secret:"O5B9rbgbAB9Gghbsp3y9HB95DAfiNs7YaR3Cj7IsTG5cm"
+//    consumer_key:       "mmsnjhnmeqaFOUkUTC46ONDBZ",
+//    consumer_secret:    "A0FS8xGk5GIHoaI8hzw5MJOyhgiD0B3vd6MRdiagDudZjbV0qD",
+//    access_token:       "2430470406-M0spmbsyfz3G1qY7YCZYR0Ye38CXeHQxNfLyqQo",
+//    access_token_secret:"O5B9rbgbAB9Gghbsp3y9HB95DAfiNs7YaR3Cj7IsTG5cm"
 });
 insertTweet=function(tweet){
     if(Tweets.find({"tweet.id":tweet.id}).count()){
